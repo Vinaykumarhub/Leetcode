5c8e30a527beeb50791ab7882b4d652744651ed6
Vinay kumar <100996400+Vinaykumarhub@users.noreply.github.com>
2024-12-02 19:53:31 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (15.71%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_map<int,int>mp;
+       /* unordered_map*/
+     //n=18
+        unordered_map<int,int>map;
+        /*
+        1. n update sum
+        19 => 82 n will store again 82 sum=0
+        82 => 68 n will store again 68 sum=0
+        68=>100 n will stores again 100 sum=0*/
+        /*
+        2. if(sum != map)
+        return false;*/
+        /*n=19
+        n%10=9
+        n=n/10;
+        n=1
+        n%10=1;
+        */
         int sum=0;
         while(sum!=1){
             sum=0;
-           while(n>0){
-            sum+=pow(n%10,2);
-            n=n/10;
+            while(n>0){
+                sum+=pow(n%10,2);
+                n=n/10;
             }
-    if(mp.find(sum)!=mp.end()){
-         return false;
-      }
-        mp[sum]++;
-        n=sum;
-        
+            if(map.find(sum)!=map.end()){
+           return false; 
+            }
+            map[sum]++;
+            n=sum;
         }
         return true;
-       
-            
+        
     }
 };
